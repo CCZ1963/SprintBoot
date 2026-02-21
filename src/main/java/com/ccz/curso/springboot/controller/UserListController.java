1b5f2f40535fcea195de1b41c8cec86e509386cb
CECZ <cesarcantelli@gmail.com>
2026-02-21 13:29:53 -0300

    Pasando lista con ModelAttribute - Video 25

--- src/main/java/com/ccz/curso/springboot/controller/UserListController.java
+++ src/main/java/com/ccz/curso/springboot/controller/UserListController.java
@@ -4,6 +4,7 @@ import com.ccz.curso.springboot.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -35,15 +36,33 @@ public class UserListController {
     @GetMapping("/list2") // http://localhost:8080/list2
     public String list2(ModelMap model) { // Con Model pasamos información
 
-        List<User> users = Arrays.asList(new User("Ricardo", "Pantoja", "rpantoja@email.com"),
-                new User("Javier", "Narvaez"),
+        List<User> users = Arrays.asList(new User("Tito", "Navarro", "navarrot@email.com"),
+                new User("Iris", "Fernández"),
                 new User("Teresa", "Bueno", "tebueno@email.com"),
-                new User("Manuel", "Duco", "ducomanuel@email.com"));
+                new User("Stefan", "Kross", "skross@email.com"));
 
         model.addAttribute("title", "Listado de Usuarios");
         model.addAttribute("users", users);
         // Llama a la pag. web de ~/templates/list_user.html
         return "list_user"; // Mismo nombre que la página web que se va a llamar, debe existir
+    }
+
+    @GetMapping("/list3") // http://localhost:8080/list3
+    public String list3(ModelMap model) { // Con Model pasamos información
+
+        model.addAttribute("title", "Listado de Usuarios");
+        // Los user los envía a través del método enviarLista
+        // Llama a la pag. web de ~/templates/list_user.html
+        return "list_user"; // Mismo nombre que la página web que se va a llamar, debe existir
+    }
+
+    @ModelAttribute("users")
+    public List<User> enviarLista(){
+        List<User> users = Arrays.asList(new User("Sara", "mendoza", "saramendoza@email.com"),
+                new User("Alicia", "Vivanco"),
+                new User("César", "Palacios", "cpalacios@email.com"),
+                new User("Mirtha", "Juliaca", "juliacam@email.com"));
+        return users;
     }
 
 }
