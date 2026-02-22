c902b5d86c193eae96b271f76ecab6c120de7480
CECZ <cesarcantelli@gmail.com>
2026-02-22 11:18:22 -0300

    Obtener varios Parámetros en la URL con @PathVariable - Video 30

--- src/main/java/com/ccz/curso/springboot/controller/PathVariableController.java
+++ src/main/java/com/ccz/curso/springboot/controller/PathVariableController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/var")
 public class PathVariableController {
@@ -18,5 +21,18 @@ public class PathVariableController {
         ParamDto  paramDto = new ParamDto();
         paramDto.setMessage(message);
         return paramDto;
+    }
+
+    @GetMapping("/foo1/{message}/{id}/{name}")
+    // http://localhost:8080/api/var/foo1/Vamos%20por%20todo/5624/Juan
+    // message debe llamarse igual en todo el método, no podemos cambiarle de nombre
+    public Map<String, Object> foo1(@PathVariable() String message, @PathVariable Long id, @PathVariable String name){
+        // http://localhost:8080/api/var/foo/Este%20es%20un%20mensaje%20de%20texto
+        Map<String,Object> json = new HashMap<>();
+
+        json.put("message", message);
+        json.put("id", id);
+        json.put("name", name);
+        return json;
     }
 }
