9dfaa112f19c56877588ea726d0b80447ce62186
CECZ <cesarcantelli@gmail.com>
2026-02-24 18:08:34 -0300

    Foward y redirect para redireccionar - Video 38

---
+++ src/main/java/com/ccz/curso/springboot/controller/HomeController.java
@@ -0,0 +1,25 @@
+package com.ccz.curso.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping({"", "/", "/home"})
+    public String home() {
+
+        return "forward:/detalle";
+        // return "redirect:/detalle";
+        // return "redirect:/otrodetalle";
+        // return "forward:/otrodetalle";
+        // return "redirect:/api/params/foo1";
+        /* La principal diferencia es que redirect ocurre en el navegador (cliente),
+         cambiando la URL y creando una nueva solicitud, mientras que forward sucede
+         internamente en el servidor, manteniendo la misma URL y reutilizando
+         la solicitud original. Redirect es del lado del cliente (navegador);
+         Forward es del lado del servidor.
+         */
+
+    }
+}
